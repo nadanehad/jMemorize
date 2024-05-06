2ad95ef0f0b8b8823bee295533a0f0701bca3651
Albert Maged <magedalbert21@gmail.com>
2024-05-06 06:42:18 +0300

    bug 12 Done

--- src/jmemorize/gui/swing/actions/file/AbstractExportAction.java
+++ src/jmemorize/gui/swing/actions/file/AbstractExportAction.java
@@ -1,7 +1,7 @@
 /*
  * jMemorize - Learning made easy (and fun) - A Leitner flashcards tool
  * Copyright(C) 2004-2008 Riad Djemili and contributors
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 1, or (at your option)
@@ -41,81 +41,78 @@ public abstract class AbstractExportAction extends AbstractSessionDisabledAction
     /**
      * Displays a Save As or Export dialog, and to confirm overwrites,
      * and to attach specified file extension.
-     * 
+     *
      * @return the file path or <code>null</code> if the dialog was cancelled.
-     * 
+     *
      * @author Perry (elsapo)
      * @author djemili
      */
-    public static File showSaveDialog(JFrame frame, ExtensionFileFilter fileFilter)
-    {
+    public static File showSaveDialog(JFrame frame, ExtensionFileFilter fileFilter) {
         JFileChooser chooser = new JFileChooser();
-        
-        try 
-        {
+
+        try {
             chooser.setCurrentDirectory(Settings.loadLastDirectory());
-        }
-        catch (Exception ioe)
-        {
+        } catch (Exception ioe) {
             Main.logThrowable("Could not load last directory", ioe);
             chooser.setCurrentDirectory(null);
-        }                
-        
+        }
+
         chooser.setFileFilter(fileFilter);
-        
+        File file = null;
+        boolean confirmed = false;
         // Loop so we can prompt again if they choose not to overwrite
-        while (true)
-        {
+        while (!confirmed) {
             // Do the actual Save As prompt
             int choice = chooser.showSaveDialog(frame);
             if (choice != JFileChooser.APPROVE_OPTION)
-                return null;
+                break;
 
-            File file = chooser.getSelectedFile();
+            file = chooser.getSelectedFile();
 
             // Attach desired extension, if supplied
             String extension = fileFilter.getExtension();
-            if (extension.length() > 0 && !file.getName().endsWith(extension))
-            {
+            if (extension.length() > 0 && !file.getName().endsWith(extension)) {
                 file = new File(file.getAbsolutePath() + '.' + extension);
                 chooser.setSelectedFile(file);
             }
-            
-            if (file.exists())
-            {
+
+            if (file.exists()) {
                 // Prompt to confirm they actually want to overwrite existing file
                 String text = Localization.get("MainFrame.CONFIRM_OVERWRITE");
                 String title = Localization.get("MainFrame.CONFIRM_OVERWRITE_TITLE");
-                
-                int act = JOptionPane.showConfirmDialog(frame, 
-                    text + " " + file.toString(), 
-                    title,
-                    JOptionPane.YES_NO_OPTION);
-                
+
+                int act = JOptionPane.showConfirmDialog(frame,
+                        text + " " + file.toString(),
+                        title,
+                        JOptionPane.YES_NO_OPTION);
+
                 if (act == JOptionPane.NO_OPTION)
                     continue;
             }
-            
+            confirmed = true;
+        }
+
+        if (confirmed) {
             Settings.storeLastDirectory(file);
             return file;
+        } else {
+            return null;
         }
     }
-    
-
     /* (non-Javadoc)
      * @see java.awt.event.ActionListener
      */
     public void actionPerformed(ActionEvent event)
     {
         Main main = Main.getInstance();
-        
-        File file = null;
+
+         File file = null;
         try
         {
             file = showSaveDialog(main.getFrame(), getFileFilter());
             if (file != null)
                 doExport(main.getLesson(), file);
-            
+
         }
         catch (IOException e)
         {
@@ -128,7 +125,7 @@ public abstract class AbstractExportAction extends AbstractSessionDisabledAction
             new ErrorDialog(main.getFrame(), msg, e).setVisible(true);
         }
     }
-    
+
     abstract protected void doExport(Lesson lesson, File file) throws IOException;
     abstract protected ExtensionFileFilter getFileFilter();
 }
